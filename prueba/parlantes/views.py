4e8279cea8553fc00af63bf3a5d3ecaf6e83ab3a
unknown <franco.orellanajara@gmail.com>
2020-10-24 13:49:16 -0300

    se añadio la vista base

--- prueba/parlantes/views.py
+++ prueba/parlantes/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+def base(requeest):
+    print("Estamos en la vista base..")
+    context = {}
+    return render(requeest, 'parlantes/base.html', context)
