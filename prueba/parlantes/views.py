ba586d8f820718baf988540d43d8e7ccd0d1a29b
unknown <franco.orellanajara@gmail.com>
2020-10-26 17:43:36 -0300

    Se añadio la vista inicio

--- prueba/parlantes/views.py
+++ prueba/parlantes/views.py
@@ -5,3 +5,8 @@ def base(requeest):
     print("Estamos en la vista base..")
     context = {}
     return render(requeest, 'parlantes/base.html', context)
+
+def inicio(requeest):
+    print("Estamos en la vista inicio..")
+    context = {}
+    return render(requeest, 'parlantes/index.html', context)
