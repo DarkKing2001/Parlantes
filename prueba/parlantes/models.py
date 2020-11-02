f1bcf16ab30863adfafcca002f3d4b5b3161ff2e
unknown <franco.orellanajara@gmail.com>
2020-11-02 20:33:28 -0300

    Se añadio el modelo de parlante

--- prueba/parlantes/models.py
+++ prueba/parlantes/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Parlante(models.Model):
+    id_parlante      = models.AutoField(db_column='id_alumno', primary_key=True)
+    nombre           = models.CharField(max_length=20, blank=True, null=True)
+    tipo             = models.CharField(max_length=10, blank=True, null=True)
+    foto = models.ImageField(upload_to='fotos', blank=True, null=True)
+
+    #toString
+    def __str__(self):
+        return  self.nombre+", "+self.tipo+", " + self.foto.__str__()
+
+    class Meta:
+        ordering = ["tipo"]
