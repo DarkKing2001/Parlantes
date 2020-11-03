ee0e9770d7a93412c17821073fb7006569d94a52
unknown <franco.orellanajara@gmail.com>
2020-11-02 22:00:12 -0300

    Se añadio el import del modelo de parlante

--- prueba/parlantes/views.py
+++ prueba/parlantes/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from parlantes.models import Parlante
+
 def base(requeest):
     print("Estamos en la vista base..")
     context = {}
@@ -20,3 +23,33 @@ def agregar(request):
     print("ok, estamos en la vista agregarr")
     context={}
     return render(request, 'parlantes/crud/formulario_agregar.html', context)
+
+def agregar_parlante(request):
+    print("hola  estoy en agregar_parlante...")
+    if request.method == 'POST':
+       mi_nombre = request.POST['nombre']
+       mi_tipo   = request.POST['tipo']
+       mi_foto   = request.FILES['foto']
+
+       if mi_nombre != "":
+           try:
+               parlante = Parlante()
+
+               alumno.rut = mi_rut
+               alumno.nombre = mi_nombre
+               alumno.apellido_paterno = mi_paterno
+               alumno.apellido_materno = mi_materno
+               alumno.fecha_nacimiento = mi_fechaNac
+               alumno.genero = mi_genero
+               alumno.foto = mi_foto
+
+               alumno.save()
+
+               return render(request, 'personas/mensajes/datos_grabados.html',{})
+
+           except alumno.DoesNotExist:
+               return render(request, 'personas/error/error_204.html', {})
+       else:
+           return render(request, 'personas/error/error_201.html', {})
+    else:
+        return render(request, 'personas/error/error_203.html', {})
