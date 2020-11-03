54b1d0252101247885280bee0a1791b39db3b82d
unknown <franco.orellanajara@gmail.com>
2020-11-02 23:44:41 -0300

    Se agrego la urls eliminar

--- prueba/parlantes/urls.py
+++ prueba/parlantes/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('agregar_parlante', views.agregar_parlante, name='agregar_parlante'),
     path('boton_buscar', views.boton_buscar, name='boton_buscar'),
     path('buscar_por_nombre', views.buscar_por_nombre, name='buscar_por_nombre'),
+    path('eliminar', views.eliminar, name='eliminar'),
 ]
