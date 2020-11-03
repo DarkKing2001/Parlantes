9510cbc9f33eb3273666c2566dffe3ca867b5bc2
unknown <franco.orellanajara@gmail.com>
2020-11-02 21:44:58 -0300

    Se añadio la urls de agregar

--- prueba/parlantes/urls.py
+++ prueba/parlantes/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('base', views.base, name = 'base'),
     path('tipo_parlante', views.tipo_parlante, name='tipo_parlante'),
     path('menu', views.menu, name='menu'),
+    path('agregar', views.agregar, name='agregar'),
 ]
