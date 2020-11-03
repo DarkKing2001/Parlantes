85d3bd2ce8b0d32517c3d664020378bc444b9c0f
unknown <franco.orellanajara@gmail.com>
2020-11-02 22:29:57 -0300

    Se agrego la urls de buscar_por_nombre

--- prueba/parlantes/urls.py
+++ prueba/parlantes/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('agregar', views.agregar, name='agregar'),
     path('agregar_parlante', views.agregar_parlante, name='agregar_parlante'),
     path('boton_buscar', views.boton_buscar, name='boton_buscar'),
+    path('buscar_por_nombre', views.buscar_por_nombre, name='buscar_por_nombre'),
 ]
